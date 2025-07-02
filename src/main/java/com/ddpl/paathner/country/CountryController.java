dc7a763d8b50d9717f65027645c8531f38f43e8b
vipultikhe234 <vipultikhe234@gmail.com>
2025-07-02 23:31:55 +0530

    Added validation code

--- src/main/java/com/ddpl/paathner/country/CountryController.java
+++ src/main/java/com/ddpl/paathner/country/CountryController.java
@@ -3,12 +3,14 @@ package com.ddpl.paathner.country;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ddpl.paathner.country.service.CountryService;
+import com.ddpl.paathner.country.validation.onCreate;
 
 @RestController
 @RequestMapping("/api/country")
@@ -17,7 +19,7 @@ public class CountryController {
 	CountryService countryService;
 	
 	@PostMapping("/insert_country")
-	public ResponseEntity<CountryDto> insertCountry(@ModelAttribute CountryDto countryDto){
+	public ResponseEntity<CountryDto> insertCountry(@Validated(onCreate.class)@ModelAttribute CountryDto countryDto){
 		return new ResponseEntity<>(countryService.insertCountry(countryDto),HttpStatus.CREATED);
 	}
 	
--- src/main/java/com/ddpl/paathner/country/CountryDto.java
+++ src/main/java/com/ddpl/paathner/country/CountryDto.java
@@ -3,7 +3,13 @@ package com.ddpl.paathner.country;
 import java.time.LocalDateTime;
 
 import com.ddpl.paathner.country.Country.CountryStatus;
+import com.ddpl.paathner.country.validation.onCreate;
+import com.ddpl.paathner.country.validation.onUpdate;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,12 +18,31 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CountryDto {
+	
 	private long countryId;
+	
+	@NotBlank(message="{message.COUNTRY_CODE.REQUIRED}",groups = {onCreate.class, onUpdate.class})
+	@Pattern(regexp = "^[A-Z]{2}$", message = "{message.COUNTRY_CODE.REGEX}",groups = {onCreate.class, onUpdate.class})
+    @Size(max = 2, message = "{message.COUNTRY_CODE.SIZE}",groups = {onCreate.class, onUpdate.class})
 	private String countryCode;
+	
+	@NotBlank(message="{message.MOBILE_CODE.REQUIRED}",groups = {onCreate.class,onUpdate.class})
+	@Pattern(regexp = "^\\+?\\d+$", message = "{message.MOBILE_CODE.REGEX}",groups = {onCreate.class, onUpdate.class})
+    @Size(max = 50, message = "{message.MOBILE_CODE.SIZE}",groups = {onCreate.class, onUpdate.class})
 	private String mobileCode;
+	
+	@NotBlank(message= "{message.COUNTRY_NAME.REQUIRED}",groups = {onCreate.class,onUpdate.class})
+	@Pattern(regexp = "^[\\p{L}\\s\\-().,&’']+$", message = "{message.COUNTRY_NAME.PATTERN}",groups = {onCreate.class, onUpdate.class})
+    @Size(max = 100, message = "{message.COUNTRY_NAME.SIZE}",groups = {onCreate.class, onUpdate.class})
 	private String countryName;
+	
+	@NotNull(message="{message.COUNTRY_STATUS.REQUIRED}",groups = {onCreate.class,onUpdate.class})
 	private CountryStatus countryStatus;
+	
+	
 	private LocalDateTime createdAt;
+	
+	
 	private LocalDateTime updatedAt;
 
 }
